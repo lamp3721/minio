eff73fa8f44c77e687b6b1f0f913c503e888a4fa
lamp <2932349894@qq.com>
2025-07-03 22:26:37 +0800

    feat(deployment): 添加 Docker部署支持并更新配置
    - 新增 Dockerfile 用于构建 Docker 镜像
    - 添加 docker-maven-plugin 配置到 pom.xml
    - 更新 application.yml 和 application-prod.yml 配置文件- 修改 vite.config.js 中的 API 代理地址

--- miniovue3/vite.config.js
+++ miniovue3/vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
       // '/foo': 'http://localhost:4567',
       // 选项写法
       '/api': {
-        target: 'http://localhost:8080', // 目标后端服务地址
+        target: 'http://192.168.0.7:902', // 目标后端服务地址
         changeOrigin: true, // 需要虚拟主机站点
         rewrite: (path) => path.replace(/^\/api/, '') // 重写路径：去掉路径中的/api
       },
     }
   }
-}) 
+})
