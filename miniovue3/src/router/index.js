5f54606527e8c0a2a7ff86e7d4e8642ded40dffc
lamp <2932349894@qq.com>
2025-07-05 16:38:12 +0800

    加 view 页面前缀

--- miniovue3/src/router/index.js
+++ miniovue3/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(), // 使用 HTML5 History 模式，路径更美观
+  history: createWebHistory('/view'), // 使用 HTML5 History 模式，路径更美观
   routes,
 });
 
---
+++ src/main/java/org/example/miniodemo/view/SpaRoutingController.java
@@ -0,0 +1,22 @@
+package org.example.miniodemo.view;// File: com.example.config.SpaRoutingConfiguration.java
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 解决 SPA (Single Page Application) 刷新页面404的问题。
+ * 此控制器匹配优先级较低，仅当其他 @RestController 或 @Controller
+ * 无法处理请求时，它才会生效。
+ */
+@Controller
+public class SpaRoutingController {
+
+    /**
+     * 捕获并转发后台管理应用（/admin/**）的所有前端路由。
+     * @return 内部转发到后台应用的入口 index.html
+     */
+    @RequestMapping(value = "/view/{path:[^.]*}")
+    public String forwardAdmin() {
+        return "forward:/view/index.html";
+    }
+
+}
